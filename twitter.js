6e6030caa61574b55c5cedae549c9a37f6e9152b
Sidney Bercasio <bercasio.sidney@gmail.com>
2019-05-31 17:21:13 +0800

    Done Dark Mode

--- twitter.js
+++ twitter.js
@@ -3,45 +3,130 @@ const actionDiv = document.querySelector('div[data-testid="tweet-actions"]');
 const inputBtn = document.querySelector('div[data-testid="input-button"]');
 const inputText = document.querySelector('textarea[data-testid="tweet-input"]');
 const tweetBtn = document.querySelector('button[data-testid="post-tweet"]');
-const tweetCounter = document.querySelector('span[data-testid="message-count"');
-
+const tweetCounter = document.querySelector('span[data-testid="message-count"]');
+const tweetContainer = document.querySelector('div[data-testid="tweets"]');
+const composeContainer = document.querySelector('.compose');
+const BlackBtn = document.querySelector('#BlackMode');
+const normalBtn = document.querySelector('#normalMode');
+const blackInput = document.getElementById('blackInput');
+const normalInput = document.getElementById('normalInput');
+var counter;
+var timer;
 //add event listener to the input textarea
-inputText.addEventListener('focus',toggleFocus);
-inputText.addEventListener('blur',toggleBlur);
-inputText.addEventListener('keyup',toggleBtn);
+inputText.addEventListener('focus', toggleFocus);
+inputText.addEventListener('blur', toggleBlur);
+inputText.addEventListener('keyup', toggleBtn);
 
+//add event listener to the submit button
+tweetBtn.addEventListener('click', submitBtn);
+
+//add event listener to the dark mode button
+BlackBtn.addEventListener('click', BlackMode);
+
+//add event listener to the normal mode button
+normalBtn.addEventListener('click', normaMode);
 
 actionDiv.style.display = "none"; //initialized the actions button to be hidden
+normalBtn.style.display = "none";
 
 //toggles if input textarea is focus
-function toggleFocus(){
+function toggleFocus() {
     actionDiv.style.display = "";
     inputBtn.style.display = "none";
-    inputText.style.height = "70px";
+    inputText.classList.add("expanded");
 }
 //toggles if input textarea is unfocused
-function toggleBlur(){
-    if(inputText.value) return;
+function toggleBlur() {
+    if (inputText.value) return;
     actionDiv.style.display = "none";
     inputBtn.style.display = "";
-    inputText.style.height = "34px";
-    
+    inputText.classList.remove("expanded");
+
 }
 //toggles if typing detected
-function toggleBtn(){
-    let maxInput = 280;
-    if(inputText.value!=""){
+function toggleBtn() {
+    let maxInput = 269;
+    if (inputText.value != "") {
         tweetBtn.disabled = false;
-    }else{
+    } else {
         tweetBtn.disabled = true;
     }
-    tweetCounter.textContent=maxInput-inputText.value.length;
-    if(tweetCounter.textContent<=10){
+    tweetCounter.textContent = maxInput - inputText.value.length;
+    if (tweetCounter.textContent <= 10) {
         tweetCounter.style.color = "red";
-    }else{
-        tweetCounter.style.color = "black";
+    } else {
+        tweetCounter.style.color = "#13b5f0";
     }
 }
+
+var interval =  setInterval(time, 1000);
+function time(){
+    const now = new Date();
+    return now.getSeconds();
+}
+
+//function for submitting tweet
+function submitBtn() {
+    var newTweet = `
+    <div class="tweet">
+        <div class="profile">
+            <img class="img-tweet-profile" src="img/damenleeturks.jpg" />
+        </div>
+        <div class="message">
+            <div class="posted-by">
+                <span class="display-name">Jeff</span
+                ><span class="handle">@jeffguy</span>
+                <span class="handle" id="timestamp" data-timeId="${counter}">1s ago</span>
+            </div>
+            <div class="content">
+             <p> ${inputText.value} </p>
+            </div>
+            <div class="tweet-actions">
+                <i class="far fa-comment"></i>
+                <i class="fas fa-retweet"></i>
+                <i class="far fa-heart"></i>
+                <i class="far fa-envelope"></i>
+            </div>
+        </div>
+    </div>`;
+    $('.tweets').prepend(newTweet);
+    inputText.value = '';
+    actionDiv.style.display = "none";
+    inputBtn.style.display = "";
+    inputText.classList.remove("expanded");
+    
+    //for time stamp
+    counter = tweetContainer.childElementCount;
+    if(counter>1){
+        for(var x=1;x<counter;x++){
+            timer = document.querySelector(`span[data-timeId="${x}"]`);
+            timer.innerHTML = time() + 's ago';
+        }
+    }
+}
+//toggle Dark Mode
+function BlackMode(){
+    tweetContainer.style.background = "black";
+    composeContainer.style.background = "black";
+    composeContainer.style.border = "1px solid #E7F7FD";
+    tweetContainer.style.border = "1px solid";
+    tweetContainer.style.color= "white";
+    BlackBtn.style.display = 'none';
+    normalBtn.style.display = '';
+    normalInput.checked=true;
+    
+}
+//toggle Normal Mode
+function normaMode(){
+    tweetContainer.style.background = "white";
+    composeContainer.style.background = "#E7F7FD";
+    tweetContainer.style.border = "1px solid white";
+    tweetContainer.style.color= "black";
+    BlackBtn.style.display = '';
+    normalBtn.style.display = 'none';
+    blackInput.checked=false;
+}
+
 
 
 

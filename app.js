4ce7804ed28ba6f7ebb6ab2172dde170b32983b9
marvin banton <marvin.banton@boom.camp>
2019-05-31 16:29:14 +0800

    Done Requirement 5

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ var textarea = document.querySelector('#composeInput');
 var inputbtn = document.querySelector('div[data-testid="input-button"]');
 var tweetbtn = document.querySelector('button[data-testid="post-tweet"]');
 var tweetcount = document.querySelector('span[data-testid="message-count"]');
+var tweets = document.querySelector('div[data-testid="tweets"]');
 
 textarea.addEventListener('focus', textInput);
 textarea.addEventListener('blur', showtextInput);
 textarea.addEventListener('keyup', showbtn);
+tweetbtn.addEventListener('click', addtweetbtn);
 
 action.style.display = 'none';
 
@@ -33,6 +35,8 @@ function showtextInput() {
 /*End Blur TextArea*/
  
 
+/* Toggle tweet button && character counter */
+
 function showbtn() {
     let maxinput = 269;
     if(textarea.value != "") {
@@ -48,3 +52,35 @@ function showbtn() {
             tweetcount.style.color = '#13B5F0'
         }
     }
+/* End toggle tweet button && character counter */
+
+function addtweetbtn() {
+
+var newtweet =
+        `<div class="tweet">
+            <div class="profile">
+            <img class="img-tweet-profile" src="img/damenleeturks.jpg" />
+            </div>
+            <div class="message">
+            <div class="posted-by">
+                <span class="display-name">Jeff</span
+                ><span class="handle">@jeffguy 1s ago</span>
+            </div>
+            <div class="content">
+                <p>${textarea.value}</p>
+            </div>
+            <div class="tweet-actions">
+                <i class="far fa-comment"></i>
+                <i class="fas fa-retweet"></i>
+                <i class="far fa-heart"></i>
+                <i class="far fa-envelope"></i>
+            </div>
+            </div>
+        </div>`;
+    $('.tweets').prepend(newtweet);
+
+    textarea.value = "";
+    action.style.display = "none";
+    inputbtn.style.display = "";
+    textarea.classList.remove('expanded');
+}

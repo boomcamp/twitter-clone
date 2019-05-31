d9140d81d79aeaa6ba8fa63553e1b0fd3bae7edc
Sidney Bercasio <bercasio.sidney@gmail.com>
2019-05-31 13:37:35 +0800

    Done requirement #3

--- twitter.js
+++ twitter.js
@@ -1,23 +1,47 @@
 
 const actionDiv = document.querySelector('div[data-testid="tweet-actions"]');
-actionDiv.style.display = "none";
 const inputBtn = document.querySelector('div[data-testid="input-button"]');
 const inputText = document.querySelector('textarea[data-testid="tweet-input"]');
+const tweetBtn = document.querySelector('button[data-testid="post-tweet"]');
+const tweetCounter = document.querySelector('span[data-testid="message-count"');
+
+//add event listener to the input textarea
+inputText.addEventListener('focus',toggleFocus);
+inputText.addEventListener('blur',toggleBlur);
+inputText.addEventListener('keyup',toggleBtn);
 
 
+actionDiv.style.display = "none"; //initialized the actions button to be hidden
+
+//toggles if input textarea is focus
 function toggleFocus(){
     actionDiv.style.display = "";
     inputBtn.style.display = "none";
     inputText.style.height = "70px";
 }
+//toggles if input textarea is unfocused
 function toggleBlur(){
     actionDiv.style.display = "none";
     inputBtn.style.display = "";
     inputText.style.height = "34px";
 }
-const tweetInput = document.querySelector('#composeInput');
-tweetInput.addEventListener('focus',toggleFocus);
-tweetInput.addEventListener('blur',toggleBlur);
+//toggles if typing detected
+function toggleBtn(){
+    let maxInput = 280;
+    if(inputText.value!=""){
+        tweetBtn.disabled = false;
+    }else{
+        tweetBtn.disabled = true;
+    }
+    tweetCounter.textContent=maxInput-inputText.value.length;
+    if(tweetCounter.textContent<=10){
+        tweetCounter.style.color = "red";
+    }else{
+        tweetCounter.style.color = "black";
+    }
+}
+
+
 
 
 

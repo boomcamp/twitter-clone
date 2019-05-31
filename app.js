d0a266d15c562268b7f15989e3858f1c517a239b
marvin banton <marvin.banton@boom.camp>
2019-05-31 15:16:54 +0800

    Done Requirement 3

--- app.js
+++ app.js
@@ -1,25 +1,44 @@
 
-const action = document.querySelector('div[data-testid="tweet-actions"]');
-const textArea = document.querySelector('#composeInput');
+var action = document.querySelector('div[data-testid="tweet-actions"]');
+var textarea = document.querySelector('#composeInput');
+var inputbtn = document.querySelector('div[data-testid="input-button"]');
+var tweetbtn = document.querySelector('button[data-testid="post-tweet"]');
+var tweetcount = document.querySelector('span[data-testid="message-count"]');
+
+textarea.addEventListener('focus', textInput);
+textarea.addEventListener('blur', showtextInput);
+textarea.addEventListener('keyup', showbtn);
 
 action.style.display = 'none';
-
-textArea.addEventListener('focus', textInput);
-textArea.addEventListener('blur', showtextInput);
 
 /*Toggle TextArea*/
 
 function textInput() {
    action.style.display = '';
-   textArea.style.height = "68px";
+   inputbtn.style.display = 'none';
+   textarea.classList.add('expanded');
 }
 /*End Toggle TextArea*/
+
 
 /*Blur TextArea*/
 
 function showtextInput() {
+    if(textarea.value) return;
     action.style.display = 'none';
-    textArea.style.height = "34px";
+    inputbtn.style.display = '';
+    textarea.classList.remove('expanded');
 }
-
 /*End Blur TextArea*/
+
+
+function showbtn() {
+    let maxinput = 269;
+    if(textarea.value != "") {
+        tweetbtn.disabled = false;
+    } else {
+        tweetbtn.disabled = true;
+    } tweetcount.content = maxvalue - textarea.length {
+        
+    }
+}

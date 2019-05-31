aa1367e9bb4e3d1618fe43ecab49468933be8cd0
judesama <judeagagad@gmail.com>
2019-05-31 14:02:22 +0800

    7 passed

---
+++ scripts/app.js
@@ -0,0 +1,84 @@
+
+var composeInput = document.querySelector('#composeInput');
+var actions = document.querySelector('.actions');
+var postBtn = document.querySelector('.post-tweet');
+var imgProfile = document.querySelector('.img-profile');
+actions.style.display = 'none';
+
+
+
+composeInput.addEventListener('focus' , showComposer);
+
+composeInput.addEventListener('keyup' , tweetCompose);
+composeInput.addEventListener('blur' , hideComposer);
+
+
+var postedTweetsArr = [];
+function postedTweets(imgSrc , displayName , handle , tweet , date){
+    this.imgSrc = imgSrc;
+    this.displayName = displayName;
+    this.handle = handle;
+    this.tweet = tweet;
+    this.date = date;
+}
+
+
+
+postBtn.addEventListener('click' , addTweet);
+
+
+
+
+
+function showComposer(event){
+    composeInput.style.height = '70px';
+    actions.style.display = 'flex';
+
+}
+
+function hideComposer(){
+    composeInput.style.height = '34px';
+    actions.style.display = 'none';
+}
+
+function tweetCompose(event){
+    
+    var composedLength = composeInput.value.length;
+    var msgCount = document.querySelector('.message-count');
+    var maxLength = composeInput.maxLength;
+    if( composedLength > 0){
+        postBtn.disabled = false;
+        let totalLength = maxLength - composedLength;
+        msgCount.innerHTML = totalLength;
+        if(composedLength > maxLength-10){
+            msgCount.classList.add('red');
+        }
+        else{
+            msgCount.classList.remove('red');
+        }
+    }
+    else{
+        postBtn.disabled = true;
+        msgCount.innerHTML = maxLength;
+    }
+}
+
+function addTweet(){
+    let imgSrc = imgProfile.src;
+    let tweet = composeInput.value;
+    let date = new Date();
+    let newTweet = new postedTweets(imgSrc , 'Jeff' , '@jeffguy' , tweet , date);
+
+    postedTweetsArr.push(newTweet);
+
+    showTweets();
+}
+
+function showTweets(){
+    
+    postedTweetsArr.forEach(element => {
+        console.log('asdas');
+    });
+
+
+}

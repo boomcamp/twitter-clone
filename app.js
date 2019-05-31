10f63c8826f229e3a304ddc1a47a85e873668029
marvin banton <marvin.banton@boom.camp>
2019-05-31 13:39:27 +0800

    Done Requirement 2

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 
 const action = document.querySelector('div[data-testid="tweet-actions"]');
+const textArea = document.querySelector('#composeInput');
 
 action.style.display = 'none';
 
+textArea.addEventListener('focus', textInput);
+textArea.addEventListener('blur', showtextInput);
 
+/*Toggle TextArea*/
+
+function textInput() {
+   action.style.display = '';
+   textArea.style.height = "68px";
+}
+/*End Toggle TextArea*/
+
+/*Blur TextArea*/
+
+function showtextInput() {
+    action.style.display = 'none';
+    textArea.style.height = "34px";
+}
+
+/*End Blur TextArea*/

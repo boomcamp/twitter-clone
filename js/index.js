757c349e7e6191e76bf6d69e5907ac16b9fd3f7b
rapraquion <rapraquion@gmail.com>
2019-10-22 04:49:29 -0400

    re-edit

--- js/index.js
+++ js/index.js
@@ -1,22 +1,17 @@
 $(document).ready(function() {
   $(".actions").hide();
-
-  //composeInput
   $("#composeInput").on("focus", function() {
     $(".input-button").hide();
     $(".actions").show();
+    $("textarea").addClass("");
 
-    //textarea
-    $("textarea").addClass("textarea.expanded");
-    if (this.value != 0) {
-      $(".actions").show();
+    if (this.value != "") {
       $(".post-tweet").removeAttr("disabled");
     }
     if (this.value > 0) {
-      280 - this.value.lenght;
+      $(".actions").show();
     }
   });
-
   $("#composeInput").on("blur", function() {
     $(".input-button").show();
     $(".actions").hide();

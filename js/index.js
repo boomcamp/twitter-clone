431eda4276658487ff7d35b218539a6b8a5af3cc
rapraquion <rapraquion@gmail.com>
2019-10-22 23:01:09 -0400

    counter beta2

--- js/index.js
+++ js/index.js
@@ -1,37 +1,36 @@
 $(document).ready(function() {
+  //initial state
   $(".actions").hide();
   $("#composeInput").on("focus", function() {
     $(".input-button").hide();
     $(".actions").show();
-    $(this).addClass("expanded");
+    $(this).addClass("expanded focus");
 
-    //textarea
-    var msgCount = document.getElementsByClassName("message-count");
-
+    //start on focus
     $(this).on("keypress", function() {
       if ($(this).val().length > 0) {
         $(".post-tweet").attr("disabled", false);
       } else {
         $(".post-tweet").attr("disabled", true);
       }
-    });
-  });
+      //message-count
+      let msgCount = $("#composeInput").attr("maxlength");
+      msgCount -= $(this).val().length;
+      $(".message-count").text(msgCount);
+      if (msgCount <= 10) {
+        $(".message-count").addClass("danger");
+      } else {
+        $(".message-count").removeClass("danger");
+      }
+    }); //on key press
+  }); //on focus
 
-  $(this).on("input", function() {
-    if (msgCount.innerHtml > 0) {
-      $(msgCount).innerText - $(this).val().length;
-    } else if (msgCount.innerHtml <= 10) {
-      msgCount.addClassList("danger");
-    } else {
-      msgCount.removeClassList("danger");
-    }
-  });
-
+  //start on blur
   $("#composeInput").on("blur", function() {
     if ($(this).val() === "") {
       $(".input-button").show();
       $(".actions").hide();
       $(this).removeClass("expanded");
     }
-  });
-});
+  }); //on blur
+}); //document.ready.function

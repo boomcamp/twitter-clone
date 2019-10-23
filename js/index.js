8fa6ad4f474e23fa976492cdc4490cad2f1a7aa8
rapraquion <rapraquion@gmail.com>
2019-10-23 01:29:29 -0400

    solved prob. #1-#7

--- js/index.js
+++ js/index.js
@@ -4,10 +4,10 @@ $(document).ready(function() {
   $("#composeInput").on("focus", function() {
     $(".input-button").hide();
     $(".actions").show();
-    $(this).addClass("expanded focus");
+    $(this).addClass("expanded");
 
     //start on focus
-    $(this).on("keypress", function() {
+    $(this).on("input", function() {
       if ($(this).val().length > 0) {
         $(".post-tweet").attr("disabled", false);
       } else {
@@ -15,22 +15,54 @@ $(document).ready(function() {
       }
       //message-count
       let msgCount = $("#composeInput").attr("maxlength");
-      msgCount -= $(this).val().length;
-      $(".message-count").text(msgCount);
+      let count = msgCount - $(this).val().length;
+      $(".message-count").text(count);
       if (msgCount <= 10) {
         $(".message-count").addClass("danger");
       } else {
         $(".message-count").removeClass("danger");
       }
     }); //on key press
-  }); //on focus
+  }); //on focus end
+
+  //on click tweet post
+  $(".post-tweet").on("click", function() {
+    let post = `<div class="tweet">
+    <div class="profile">
+      <img class="img-tweet-profile" src="img/funwatercat.jpg" />
+    </div>
+    <div class="message">
+      <div class="posted-by">
+        <span class="display-name">Michael</span>
+        <span class="handle">@sometwitterguy</span>
+      </div>
+      <div class="content">
+        <p>${$("textarea[id*='composeInput']").val()}</p>
+      </div>
+      <div class="tweet-actions">
+        <i class="far fa-comment"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="far fa-heart"></i>
+        <i class="far fa-envelope"></i>
+      </div>
+    </div>
+  </div>`;
+    $(".tweets").prepend(post);
+    $(".actions").hide();
+    $(".input-button").show();
+    $("#composeInput")
+      .removeClass("expanded")
+      .val("");
+    $(".message-count").text("280");
+    $(".post-tweet").attr("disabled", true);
+  });
 
   //start on blur
   $("#composeInput").on("blur", function() {
     if ($(this).val() === "") {
       $(".input-button").show();
       $(".actions").hide();
-      $(this).removeClass("expanded");
+      $("#composeInput").removeClass("expanded");
     }
-  }); //on blur
-}); //document.ready.function
+  }); //on blur end
+}); //document end

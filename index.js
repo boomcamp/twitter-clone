2e99c22d84972af46cf3f763884eb69ebf02b448
Martha Dansyle Marbella <martha.marbella@boom.camp>
2019-10-23 13:23:31 +0800

    Updated

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 $("document").ready(function() {
+   var max = parseInt($("textarea").attr("maxlength"));
+
    $(".actions").hide();
    $(".input-container textarea").blur(function() {
       if ($(".input-container textarea").val() == "") {
@@ -7,13 +9,16 @@ $("document").ready(function() {
          $(".input-button").show();
       }
    });
+
    $(".input-container textarea").focus(function() {
       $(".actions").show();
       $(".input-button").hide();
-      $("textarea").on("keyup", tweet);
+      $("textarea").on("keypress input", tweet);
       $(".input-container textarea").addClass("expanded");
-      $("textarea").keyup(count);
+      $("textarea").on("keypress input", count);
    });
+
+   $("#dark-mode").on("click", darkMode);
 
    function tweet() {
       if ($(this).val() != "") {
@@ -24,16 +29,21 @@ $("document").ready(function() {
    }
 
    function count() {
-      var tweetContent = $(this).val().length;
-      $(".message-count").text(280 - tweetContent);
+      $(".message-count").text(max);
+      var total = max - $("textarea").val().length;
 
-      if (tweetContent > 280 || tweetContent <= 0) {
-         $(".post-tweet").attr("disabled", true);
-      }
-      if (tweetContent <= 280 && tweetContent >= 270) {
+      $(".message-count").text(total);
+
+      if (total <= 10) {
          $(".message-count").addClass("danger");
       } else {
          $(".message-count").removeClass("danger");
+      }
+
+      if ($("textarea").val() != "") {
+         $(".post-tweet").attr("disabled", false);
+      } else {
+         $(".post-tweet").attr("disabled", true);
       }
    }
 
@@ -42,17 +52,32 @@ $("document").ready(function() {
    function input() {
       var tweet = $("textarea").val();
       $(".tweets").prepend(
-         '<div class="tweet"><div class="profile"><img class="img-tweet-profile" src="img/damenleeturks.jpg" /></div><div class="message"><div class="posted-by"><span class="display-name">Jeff</span><span class="handle">@damenleeturks</span></div><div class="content"><p>' +
+         '<div class="tweet"><div class="profile"><img class="img-tweet-profile" src="img/jennyshen.jpg" /></div><div class="message"><div class="posted-by"><span class="display-name">Jenny</span><span class="handle">@jennyshen</span></div><div class="content"><p>' +
             tweet +
             '</p></div><div class="tweet-actions"><i class="far fa-comment"></i><i class="fas fa-retweet"></i><i class="far fa-heart"></i><i class="far fa-envelope"></i></div></div></div>'
       );
-      $(".message-count").text(280);
+
       $("textarea").val("");
       $(".actions").hide();
       $(".input-container textarea").removeClass("expanded");
       $(".input-button").show();
       if ($("textarea").val() == "") {
          $(".post-tweet").attr("disabled", true);
+      }
+      $(".message-count").text(280);
+   }
+
+   function darkMode() {
+      if ($("input[type=checkbox]").prop("checked")) {
+         $(".compose").addClass("dark-color");
+         $(".tweets").addClass("dark-color");
+         $(".toggle-switch").addClass("dark-color border-top");
+         $(".dMode-text").text("LIGHT MODE");
+      } else {
+         $(".compose").removeClass("dark-color");
+         $(".tweets").removeClass("dark-color");
+         $(".toggle-switch").removeClass("dark-color border-top");
+         $(".dMode-text").text("DARK MODE");
       }
    }
 });
